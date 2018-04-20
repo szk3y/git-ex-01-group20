3f79582e96dc3c3dc731ce29c3a138bee504ac8c
szk3y <d4tt423@gmail.com>
2018-04-20 19:03:51 +0900

    Fix daysofmonth to show correct number

--- DaysOfMonth.java
+++ DaysOfMonth.java
@@ -15,6 +15,21 @@ public class DaysOfMonth {
         month, year, daysofmonth(month, year));
   }
   static int daysofmonth(int month, int year) {
-    return 30;
+    int[] days = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    if(isLeapYear(year) && month == 2) {
+      return 29;
+    }
+    return days[month-1];
+  }
+  static boolean isLeapYear(int year) {
+    if(year % 400 == 0) {
+      return true;
+    } else if(year % 100 == 0) {
+      return false;
+    } else if(year % 4 == 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
