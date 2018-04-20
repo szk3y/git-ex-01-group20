454e91040a8eac67ead93ed616a900331f76f061
szk3y <d4tt423@gmail.com>
2018-04-20 19:08:45 +0900

    Update DaysOfMonth.java

--- DaysOfMonth.java
+++ DaysOfMonth.java
@@ -10,7 +10,6 @@ public class DaysOfMonth {
   public static void main(String[] args){
     int year = Integer.parseInt(args[0]);
     int month = Integer.parseInt(args[1]);
-    // Buggy output
     System.out.printf("Month of %d/%d has %d days\n",
         month, year, daysofmonth(month, year));
   }
